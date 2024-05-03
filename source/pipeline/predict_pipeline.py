8a0aeacb93b91525a24898f218ac2c6df0bd1656
dyavadi8769 <sdyavadi@tamu.edu>
2024-05-02 22:51:10 -0500

    Model is built successfully

--- source/pipeline/predict_pipeline.py
+++ source/pipeline/predict_pipeline.py
@@ -20,3 +20,44 @@ class PredictPipeline:
         
         except Exception as e:
             raise CustomException(e,sys)
+
+class CustomData:
+    def __init__(  self,
+        gender: str,
+        race_ethnicity: str,
+        parental_level_of_education,
+        lunch: str,
+        test_preparation_course: str,
+        physics_score: int,
+        chemistry_score: int):
+
+        self.gender = gender
+
+        self.race_ethnicity = race_ethnicity
+
+        self.parental_level_of_education = parental_level_of_education
+
+        self.lunch = lunch
+
+        self.test_preparation_course = test_preparation_course
+
+        self.physics_score = physics_score
+
+        self.chemistry_score = chemistry_score
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race_ethnicity": [self.race_ethnicity],
+                "parental_level_of_education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test_preparation_course": [self.test_preparation_course],
+                "physics_score": [self.physics_score],
+                "chemistry_score": [self.chemistry_score],
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            raise CustomException(e, sys)
